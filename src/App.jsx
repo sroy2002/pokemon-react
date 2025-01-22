65abd6739a030124dad26de421effb613118b7a4
Suranjana Roy <roysupra75@gmail.com>
2025-01-22 23:09:55 +0530

    added api

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,42 @@
-
-import './App.css'
-
+import api_url from "./api"
+import Card from "./components/Card";
+import { useEffect, useState } from "react";
 function App() {
-
-
+  const [pokemonCard, setPokemonCard] = useState([]);
+  useEffect(()=>{
+    const fetchPokemon = async () =>{
+      const response = await fetch(api_url);
+      const data = await response.json();
+      const details = await Promise.all(
+        data.results.map(async (pokemon)=>{
+          const pokemonDetails = await fetch(pokemon.url);
+          return pokemonDetails.json();
+        })
+      );
+      setPokemonCard(details);
+    };
+    fetchPokemon();
+  },[]);
+  
   return (
-    <>
-      <h1 className='font-bold text-3xl'>HELLO WORLD</h1>
-    </>
-  )
+    <div >
+      <div className=" m-4 flex justify-center items-center">
+        <h1 className="font-bold text-3xl text-white">POKEMON CARDS</h1>
+      </div>
+        {console.log(pokemonCard)}
+      <div>
+        {
+          pokemonCard.map((pokemon)=>(
+            <div>
+              <img src={pokemon.sprites.front_default} alt="" />
+            </div>
+
+          ))
+         
+        }
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
